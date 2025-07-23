55fa4a5fc2f03b92790dde1fe249e3f58f0b754a
Meshgenerali <meshackmuuo595@gmail.com>
2025-07-24 01:23:00 +0300

    created home page and jobs components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCard from "./components/HomeCard";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import Jobs from "./pages/Jobs";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<Jobs />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCard />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
